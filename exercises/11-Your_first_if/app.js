fd03554e91083caa2013148875a067573923c2fd
CoolPenwin <alberto.romero.martin.93@gmail.com>
2024-06-12 14:36:22 +0000

    Pending changes exported from your codespace

--- exercises/11-Your_first_if/app.js
+++ exercises/11-Your_first_if/app.js
@@ -1,3 +1,26 @@
-let total = prompt('How many km are left to go?');
+let total = prompt ( "falta weeeey?");
+if (total<= 50){
+    console.log ("ya llemaos")
+}else if(total<= 100){
+    console.log("un pocoooo")
+}else {
+    console.log("si tio falta mucho")
+}
 
-// Your code below:
+
+
+
+
+
+
+
+
+//* Your code below:
+//if (total <= 50) {
+//    console.log("I'm parking. I'll see you right now");
+//} else if (total <= 100) {
+//    console.log("We'll be there in 5 minutes");
+//} else {
+//    console.log("We still have a bit of driving left to go");
+//}
+//
